23c3782e105b553e29b36ae774ad28a682b6904a
Trinath <tkondapalli@g1.global>
2026-07-19 19:17:44 +0530

    Fix PostCSS config to restore Tailwind styles

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,4 +1,7 @@
 // Note: Using Tailwind CSS v4 via Vite plugin, this file is included for architectural compliance.
 export default {
-  plugins: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
